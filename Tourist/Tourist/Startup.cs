ec1537e505ba5551ec1e4dacc280f6aa17271bf4
Amphizale <enforcer.snk@gmail.com>
2018-11-19 21:41:00 +0300

    Создание классов модели

---
+++ Tourist/Tourist/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Tourist.Startup))]
+namespace Tourist
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
