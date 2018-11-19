0db33d32d77984258799eb28dcecd3f5be1ea64e
Amphizale <enforcer.snk@gmail.com>
2018-11-19 22:09:43 +0300

    Создание DAO и добавление логирования

---
+++ Tourist/Tourist/Models/Logger.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using log4net;
+using log4net.Config;
+
+namespace Tourist.Models
+{
+    public class Logger
+    {
+        static Logger()
+        {
+            XmlConfigurator.Configure();
+        }
+
+
+        public static ILog For(object LoggedObject)
+        {
+            if (LoggedObject != null)
+                return For(LoggedObject.GetType());
+            else
+                return For(null);
+        }
+
+        public static ILog For(Type ObjectType)
+        {
+            if (ObjectType != null)
+                return LogManager.GetLogger(ObjectType.Name);
+            else
+                return LogManager.GetLogger(string.Empty);
+        }
+    }
+}
